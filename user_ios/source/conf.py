280488185503b77978270769790fff4df471f678
Tewodrosp <90536285+Tewodrosp@users.noreply.github.com>
2022-08-17 21:24:37 -0400

    Update conf.py

--- user_ios/source/conf.py
+++ user_ios/source/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'contents'
 
 # General information about the project.
-project = 'CentreStack iOS User Guide'
-copyright = '2021 - Gladinet, Inc. All Rights Reserved'
-author = 'Gladinet, Inc.'
+project = 'Shareorigin iOS User Guide'
+copyright = '2021 - Shareorigin LLC All Rights Reserved'
+author = 'Shareorigin LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -226,8 +226,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'CentreStackiOSUserGuide.tex', 'CentreStack iOS User Guide Documentation',
-   'Gladinet, Inc.', 'manual'),
+  (master_doc, 'CentreStackiOSUserGuide.tex', 'Shareorigin iOS User Guide Documentation',
+   'Shareorigin LLC', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -256,7 +256,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'centrestackiosuserguide', 'CentreStack iOS User Guide Documentation',
+    (master_doc, 'centrestackiosuserguide', 'Shareorigin iOS User Guide Documentation',
      [author], 1)
 ]
 
@@ -270,7 +270,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'CentreStackiOSUserGuide', 'CentreStack iOS User Guide Documentation',
+  (master_doc, 'CentreStackiOSUserGuide', 'Shareorigin iOS User Guide Documentation',
    author, 'CentreStackiOSUserGuide', 'One line description of project.',
    'Miscellaneous'),
 ]
