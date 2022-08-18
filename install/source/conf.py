d365efbb0a9e6ca38761cca0f7b2e1fe91dc7d2b
Tewodrosp <90536285+Tewodrosp@users.noreply.github.com>
2022-08-17 21:07:50 -0400

    Update conf.py

--- install/source/conf.py
+++ install/source/conf.py
@@ -210,7 +210,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ShareoriginInstallationGuidedoc'
+htmlhelp_basename = 'CentreStackInstallationGuidedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -232,7 +232,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'ShareoriginInstallationGuide.tex', 'Shareorigin Installation Guide Documentation',
+  (master_doc, 'CentreStackInstallationGuide.tex', 'Shareorigin Installation Guide Documentation',
    'Shareorigin LLC', 'manual'),
 ]
 
@@ -262,7 +262,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'shareorigininstallationguide', 'Shareorigin Installation Guide Documentation',
+    (master_doc, 'centreStackinstallationguide', 'Shareorigin Installation Guide Documentation',
      [author], 1)
 ]
 
@@ -276,8 +276,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'ShareoriginInstallationGuide', 'Shareorigin Installation Guide Documentation',
-   author, 'ShareoriginInstallationGuide', 'One line description of project.',
+  (master_doc, 'CentreStackInstallationGuide', 'Shareorigin Installation Guide Documentation',
+   author, 'CentreStackInstallationGuide', 'One line description of project.',
    'Miscellaneous'),
 ]
 
